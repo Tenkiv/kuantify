3745339ac147b8aae786f6946b6af2a0e983a1fc
Zack Juhasz <zjuhasz@tenkiv.com>
2017-07-17 19:46:06 -0700

    Modified Output again.

--- src/main/kotlin/org/tenkiv/daqc/hardware/definitions/channel/Output.kt
+++ src/main/kotlin/org/tenkiv/daqc/hardware/definitions/channel/Output.kt
@@ -6,38 +6,33 @@ import org.tenkiv.daqc.DaqcQuantity
 import org.tenkiv.daqc.DaqcValue
 import org.tenkiv.daqc.hardware.definitions.Updatable
 
-typealias QuantityOutput<Q> = Output<DaqcQuantity<Q>>
-typealias BinaryStateOutput = Output<BinaryState>
+typealias StandardQuantityOutput<Q> = Output<DaqcQuantity<Q>, DaqcQuantity<Q>>
+typealias StandardBinaryStateOutput = Output<BinaryState, BinaryState>
 
-typealias StandardQuantityOutput<Q> = StandardOutput<DaqcQuantity<Q>>
-typealias StandardBinaryStateOutput = StandardOutput<BinaryState>
+typealias AnyQuantityOutput<Q> = Output<DaqcQuantity<Q>, out DaqcValue>
+typealias AnyBinaryStateOutput = Output<BinaryState, out DaqcValue>
+typealias AnyQuantityOnlyOutput<Q> = Output<DaqcQuantity<Q>, out DaqcQuantity<*>>
 
 /**
- * Base interface for a control output. Should rarely be used directly, in most cases [StandardOutput] should be used.
+ * Most abstract representation of an output to control a physical property. It should usually be accessed through one
+ * of its type aliases.
  *
- * @param T The type this output controls.
+ * @param P The "primary" type controlled by this output. If this interface is accessed directly this is the only type
+ * that can be set.
+ *
+ * @param U The type of the updates on what value was just sent out by this output. The reason it may be different from
+ * [P] is the implementing class may have options to set more types than just [P], which we may want to receive updates
+ * on even though we cannot set the output to said type when accessing the interface directly.
+ *
+ * There are only two practical settings for this are [DaqcQuantity] of * and [DaqcValue]. If [U] is not set to one of
+ * these types implement the [StandardQuantityOutput] or [StandardBinaryStateOutput] type alias.
  */
-interface Output<in T : DaqcValue> {
+interface Output<in P : DaqcValue, U : DaqcValue> : Updatable<ValueInstant<U>> {
 
     val isActive: Boolean
 
-    fun setOutput(setting: T)
+    fun setOutput(setting: P)
 
     fun deactivate()
 
 }
-
-/**
- *
- * @param T The type this output controls.
- */
-interface StandardOutput<T : DaqcValue> : Output<T>, Updatable<ValueInstant<T>>
-
-/**
- * An [Updatable] output whose implementing class can output multiple different types of DaqcValues. This interface
- * should only be used when you're sure the abstraction cannot be achieved with one or multiple [StandardOutput]s.
- *
- * @param P The "primary" type outputted by this Output. This is the only type that can be set if this is seen as an
- * [Output] or a [MultiOutput]. To set any potential output type, access lower abstraction directly.
- */
-interface MultiOutput<in P : DaqcValue> : Output<P>, Updatable<ValueInstant<DaqcValue>>
