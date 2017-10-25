d41a9fe23df486540993b8ca1c2f8fb7ded67243
Zack Juhasz <zjuhasz@tenkiv.com>
2017-10-24 19:52:38 -0700

    Added setOutput extension that allows passing ComparableQuantity and automatically converts it to DaqcQuantity.

--- src/main/kotlin/org/tenkiv/daqc/Output.kt
+++ src/main/kotlin/org/tenkiv/daqc/Output.kt
@@ -1,9 +1,13 @@
 package org.tenkiv.daqc
 
 import org.tenkiv.coral.ValueInstant
+import tec.uom.se.ComparableQuantity
+import javax.measure.Quantity
 
 typealias QuantityOutput<Q> = Output<DaqcQuantity<Q>>
 typealias BinaryStateOutput = Output<BinaryState>
+
+fun <Q : Quantity<Q>> QuantityOutput<Q>.setOutput(setting: ComparableQuantity<Q>) = setOutput(setting.toDaqc())
 
 interface Output<T : DaqcValue> : Updatable<ValueInstant<T>> {
 
