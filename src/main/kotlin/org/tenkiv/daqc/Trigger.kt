8854126d8a9dad038169833572b2f436b3882dda
Zack Juhasz <zjuhasz@tenkiv.com>
2017-12-05 17:29:12 -0800

    * Fixed bug with recording duration in memory. * Tweaked Trigger names slightly.

--- src/main/kotlin/org/tenkiv/daqc/Trigger.kt
+++ src/main/kotlin/org/tenkiv/daqc/Trigger.kt
@@ -5,20 +5,20 @@ import org.tenkiv.coral.ValueInstant
 import java.util.concurrent.atomic.AtomicInteger
 
 class Trigger<T : DaqcValue>(val triggerOnSimultaneousValues: Boolean = false,
-                             val maxTimesTriggered: MaxTriggerCount = TriggerCount(1),
+                             val maxTriggerCount: MaxTriggerCount = MaxTriggerCount.Limited(1),
                              vararg triggerConditions: TriggerCondition<T>,
                              triggerFunction: () -> Unit) {
 
     constructor(vararg triggerConditions: TriggerCondition<T>,
-                triggerFunction: () -> Unit):
+                triggerFunction: () -> Unit) :
             this(false,
-                    TriggerCount(1),
+                    MaxTriggerCount.Limited(1),
                     *triggerConditions,
                     triggerFunction = triggerFunction)
 
     constructor(maxTimesTriggered: MaxTriggerCount,
                 vararg triggerConditions: TriggerCondition<T>,
-                triggerFunction: () -> Unit):
+                triggerFunction: () -> Unit) :
             this(false,
                     maxTimesTriggered,
                     *triggerConditions,
@@ -26,26 +26,26 @@ class Trigger<T : DaqcValue>(val triggerOnSimultaneousValues: Boolean = false,
 
     constructor(triggerOnSimultaneousValues: Boolean,
                 vararg triggerConditions: TriggerCondition<T>,
-                triggerFunction: () -> Unit):
+                triggerFunction: () -> Unit) :
             this(triggerOnSimultaneousValues,
-                    TriggerCount(1),
+                    MaxTriggerCount.Limited(1),
                     *triggerConditions,
                     triggerFunction = triggerFunction)
 
     private val channelList: MutableList<SubscriptionReceiveChannel<ValueInstant<T>>> = ArrayList()
 
     fun stop() {
-        if (maxTimesTriggered is TriggerCount && maxTimesTriggered.atomicCount.get() > 0) {
-            maxTimesTriggered.atomicCount.decrementAndGet()
+        if (maxTriggerCount is MaxTriggerCount.Limited && maxTriggerCount.atomicCount.get() > 0) {
+            maxTriggerCount.atomicCount.decrementAndGet()
 
-            if (maxTimesTriggered.atomicCount.get() <= 0){
+            if (maxTriggerCount.atomicCount.get() <= 0) {
                 channelList.forEach { it.close() }
             }
         }
     }
 
     init {
-        if (!(maxTimesTriggered is TriggerCount && maxTimesTriggered.atomicCount.get() == 0)) {
+        if (!(maxTriggerCount is MaxTriggerCount.Limited && maxTriggerCount.atomicCount.get() == 0)) {
             triggerConditions.forEach {
                 channelList.add(it.input.broadcastChannel.consumeAndReturn { update ->
                     val currentVal = update
@@ -60,11 +60,11 @@ class Trigger<T : DaqcValue>(val triggerOnSimultaneousValues: Boolean = false,
                             triggerConditions.all {
                                 val value = it.lastValue ?: return@all false
                                 it.condition(value)
-                            }.apply { triggerFunction.invoke() }
+                            }.apply { triggerFunction() }
 
                         } else {
                             stop()
-                            triggerConditions.all { it.hasBeenReached }.apply { triggerFunction.invoke() }
+                            triggerConditions.all { it.hasBeenReached }.apply { triggerFunction() }
                         }
                     }
                 })
@@ -73,10 +73,14 @@ class Trigger<T : DaqcValue>(val triggerOnSimultaneousValues: Boolean = false,
     }
 }
 
-sealed class MaxTriggerCount
+sealed class MaxTriggerCount {
+    data class Limited(val totalCount: Int) : MaxTriggerCount() {
 
-data class TriggerCount(val count: Int): MaxTriggerCount(){
-    internal val atomicCount = AtomicInteger(count)
+        val remainingCount get() = atomicCount.get()
+
+        internal val atomicCount = AtomicInteger(totalCount)
+
+    }
+
+    object Unlimited : MaxTriggerCount()
 }
-
-class UnlimitedCount: MaxTriggerCount()
