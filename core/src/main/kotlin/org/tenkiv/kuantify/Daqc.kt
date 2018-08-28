822cb174d77e3f2957c94f9c95476ccc556a1539
Zack Juhasz <zjuhasz@tenkiv.org>
2018-08-29 00:34:14 +0100

    Updated daqcThreadContext documentation

--- core/src/main/kotlin/org/tenkiv/kuantify/Daqc.kt
+++ core/src/main/kotlin/org/tenkiv/kuantify/Daqc.kt
@@ -34,7 +34,8 @@ typealias QuantityMeasurement<Q> = ValueInstant<DaqcQuantity<Q>>
  * The default context which the library uses to handle data.
  * This coroutine context uses only a single thread, thus thread blocking calls and long running computations must never
  * be executed on it. It should only be used for suspending IO bound operations where the default multithreaded context
- * is not a viable option, such as when multiple coroutines share some mutable state that is not thread safe.
+ * is not a viable option, such as when multiple coroutines share some mutable state that is not thread safe. It never
+ * has to be used, another single thread context or a UI context could be used just as well if they are available.
  *
  * @see <a href="https://github.com/Kotlin/kotlinx.coroutines/blob/master/coroutines-guide.md#thread-confinement-coarse-
  * grained">Thread confinement coarse-grained</a>
