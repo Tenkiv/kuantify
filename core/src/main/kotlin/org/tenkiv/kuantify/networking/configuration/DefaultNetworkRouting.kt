51b76af0123a0dd4cb45e16b4697e878a0b46a44
Zack Juhasz <zjuhasz@tenkiv.com>
2019-02-03 08:43:50 +0000

    Got rid of deprecated usage in DefaultNetworkRouting

--- core/src/main/kotlin/org/tenkiv/kuantify/networking/configuration/DefaultNetworkRouting.kt
+++ core/src/main/kotlin/org/tenkiv/kuantify/networking/configuration/DefaultNetworkRouting.kt
@@ -1,12 +1,7 @@
 package org.tenkiv.kuantify.networking.configuration
 
-import kotlinx.serialization.json.*
 import mu.*
-import org.tenkiv.coral.*
-import org.tenkiv.kuantify.data.*
 import org.tenkiv.kuantify.gate.acquire.input.*
-import org.tenkiv.kuantify.lib.*
-import org.tenkiv.kuantify.networking.*
 
 private val logger = KotlinLogging.logger {}
 
@@ -14,42 +9,11 @@ fun CombinedRouteConfig.addStandardConfig() {
 
     device.daqcGateMap.forEach { id, gate ->
         when (gate) {
-            is Input<*> -> addInputRouting(id, gate)
+            is Input<*> -> TODO()
             else -> logger.debug {
                 """Gate $id on device $device is not of a type supported by standard routing,
                     | see if it can be modified to fit a supported type, or ensure custom routing
                     | is provided for it.""".trimMargin()
-            }
-        }
-    }
-}
-
-@Suppress("UNCHECKED_CAST")
-private fun CombinedRouteConfig.addInputRouting(id: String, input: Input<*>) {
-    val thisInputRc = route(RC.DAQC_GATE, id)
-
-    route(thisInputRc + RC.VALUE) to handler(input.updateBroadcaster.openSubscription(), isFullyBiDirectional = false) {
-        sendFromHost()
-
-        serializeMessage {
-            when (it.value) {
-                is BinaryState -> {
-                    val measurement = it as ValueInstant<BinaryState>
-                    Json.stringify(ValueInstantSerializer(BinaryState.serializer()), measurement)
-                }
-                is DaqcQuantity<*> -> {
-                    val measurement = it as ValueInstant<DaqcQuantity<*>>
-                    Json.stringify(ValueInstantSerializer(ComparableQuantitySerializer), measurement)
-                }
-            }
-        } withSerializer {
-            when (input) {
-                is QuantityKuantifyRemoteInput<*> -> receiveMessageOnRemote {
-
-                }
-                is BinaryStateKuantifyRemoteInput -> receiveMessageOnRemote {
-
-                }
             }
         }
     }
