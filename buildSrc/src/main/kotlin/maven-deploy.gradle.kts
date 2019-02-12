13295b98edfc920ac1fca91e0149e32740f6434e
Shan Kern <skern@tenkiv.org>
2019-02-12 14:45:55 +0000

    Working on deploy script

--- buildSrc/src/main/kotlin/maven-deploy.gradle.kts
+++ buildSrc/src/main/kotlin/maven-deploy.gradle.kts
@@ -1,12 +1,46 @@
 plugins {
-    maven
+    `maven-publish`
     signing
 }
 
+val isSnapshot: Boolean = version.toString().contains("SNAPSHOT")
+
+val modulesList = listOf(
+    "${projectDir.absolutePath}/core",
+    "${projectDir.absolutePath}/android-core",
+    "${projectDir.absolutePath}/android-local",
+    "${projectDir.absolutePath}/learning"
+)
+
 tasks {
-    register("hello") {
-        doLast {
-            println("hello")
+    getByName<Upload>("uploadArchives") {
+        repositories {
+            withConvention(MavenRepositoryHandlerConvention::class) {
+                mavenDeployer {
+                    withGroovyBuilder {
+                        "repository"("url" to uri()) {
+                            "authentication"("userName", "bob")
+                        }
+                        "snapshotRepository"("url" to uri("$buildDir/m2/snapshots"))
+                    }
+                    pom.project {
+                        withGroovyBuilder {
+                            "parent" {
+                                "groupId"("org.gradle")
+                                "artifactId"("kotlin-dsl")
+                                "version"("1.0")
+                            }
+                            "licenses" {
+                                "license" {
+                                    "name"("The Apache Software License, Version 2.0")
+                                    "url"("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                                    "distribution"("repo")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
