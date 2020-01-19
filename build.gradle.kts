3845810e84e951bf00227c04d148b992abbc8259
Shannon Kern <me@shankern.com>
2020-01-18 23:27:18 -0800

    - cleaned up build script with util functions

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,6 @@
  */
 
 @file:Suppress("KDocMissingDocumentation", "PublicApiImplicitType")
-import java.io.FileInputStream
-import java.util.*
 
 buildscript {
     repositories {
@@ -47,19 +45,18 @@ plugins {
     signing
 }
 
-val isRelease = !version.toString().endsWith("SNAPSHOT")
-val properties = Properties()
-val propertiesFile = File(rootDir, "local.properties")
-if (propertiesFile.canRead()) {
-    properties.load(FileInputStream(propertiesFile))
-}
-
-extra["signing.keyId"] = properties.getProperty("SIGNING_KEYID")
-extra["signing.secretKeyRingFile"] = properties.getProperty("SIGNING_SECRETKEYRINGFILE")
-extra["signing.password"] = properties.getProperty("SIGNING_KEYPASSWORD")
+val isRelease = isRelease()
+val properties = createPropertiesFromLocal()
+setSigningExtrasFromProperties(properties)
 
 kotlin {
-    jvm()
+    jvm {
+        val main by compilations.getting {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
 
     sourceSets {
         /**
@@ -82,77 +79,54 @@ kotlin {
          * jvmMain == core-jvm artifact
          */
 
-        jvm {
-            val main by compilations.getting {
-                kotlinOptions {
-                    jvmTarget = "1.8"
-                }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8", Vof.kotlin))
 
-                dependencies {
-                    implementation(kotlin("stdlib-jdk8", Vof.kotlin))
+                //General kotlin utilities
+                api("org.tenkiv.coral:coral-jvm:${Vof.coral}")
+                api("io.arrow-kt:arrow-core:${Vof.arrow}")
+                implementation(kotlin("reflect", Vof.kotlin))
 
-                    //General kotlin utilities
-                    api("org.tenkiv.coral:coral-jvm:${Vof.coral}")
-                    api("io.arrow-kt:arrow-core:${Vof.arrow}")
-                    implementation(kotlin("reflect", Vof.kotlin))
+                //Coroutines
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Vof.coroutinesX}")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${Vof.coroutinesX}")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:${Vof.coroutinesX}")
 
-                    //Coroutines
-                    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Vof.coroutinesX}")
-                    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${Vof.coroutinesX}")
-                    api("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:${Vof.coroutinesX}")
+                //Logging
+                implementation("io.github.microutils:kotlin-logging:${Vof.kotlinLogging}")
 
-                    //Logging
-                    implementation("io.github.microutils:kotlin-logging:${Vof.kotlinLogging}")
+                //Serialization
+                api("org.jetbrains.kotlinx:kotlinx-serialization-runtime:${Vof.serializationX}")
 
-                    //Serialization
-                    api("org.jetbrains.kotlinx:kotlinx-serialization-runtime:${Vof.serializationX}")
+                //Units of measurement
+                api("org.tenkiv.physikal:physikal-complete-units:${Vof.physikal}")
 
-                    //Units of measurement
-                    api("org.tenkiv.physikal:physikal-complete-units:${Vof.physikal}")
+                //ktor
+                implementation("io.ktor:ktor-server-core:${Vof.ktor}")
+                implementation("io.ktor:ktor-websockets:${Vof.ktor}")
+                implementation("io.ktor:ktor-server-sessions:${Vof.ktor}")
 
-                    //ktor
-                    implementation("io.ktor:ktor-server-core:${Vof.ktor}")
-                    implementation("io.ktor:ktor-websockets:${Vof.ktor}")
-                    implementation("io.ktor:ktor-server-sessions:${Vof.ktor}")
-
-                    implementation("io.ktor:ktor-client-core-jvm:${Vof.ktor}")
-                    implementation("io.ktor:ktor-client-websockets-jvm:${Vof.ktor}")
-                }
+                implementation("io.ktor:ktor-client-core-jvm:${Vof.ktor}")
+                implementation("io.ktor:ktor-client-websockets-jvm:${Vof.ktor}")
             }
+        }
 
-            val test by compilations.getting {
-                dependencies {
-                    implementation(kotlin("reflect"))
-                    implementation(kotlin("test"))
-                    implementation("org.spekframework.spek2:spek-dsl-jvm:${Vof.spek}")
-                    implementation("io.mockk:mockk:${Vof.mockk}")
-                    implementation("io.ktor:ktor-server-test-host:${Vof.ktor}")
-                    implementation("io.ktor:ktor-client-cio:${Vof.ktor}")
-                    runtimeOnly("org.spekframework.spek2:spek-runner-junit5:${Vof.spek}")
-                    runtimeOnly("org.junit.platform:junit-platform-launcher:${Vof.junitPlatform}")
-                }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("reflect"))
+                implementation(kotlin("test"))
+                implementation("org.spekframework.spek2:spek-dsl-jvm:${Vof.spek}")
+                implementation("io.mockk:mockk:${Vof.mockk}")
+                implementation("io.ktor:ktor-server-test-host:${Vof.ktor}")
+                implementation("io.ktor:ktor-client-cio:${Vof.ktor}")
+                runtimeOnly("org.spekframework.spek2:spek-runner-junit5:${Vof.spek}")
+                runtimeOnly("org.junit.platform:junit-platform-launcher:${Vof.junitPlatform}")
             }
+        }
 
-            tasks {
-                withType<Test> {
-                    outputs.upToDateWhen { false }
-                    useJUnitPlatform {
-                        includeEngines("spek2")
-                    }
-                    testLogging.showStandardStreams = true
-                    maxHeapSize = "1g"
-                }
-
-                register<Jar>("sourcesJar") {
-                    archiveClassifier.set("sources")
-                    from(kotlin.sourceSets.commonTest)
-                }
-
-                register<Jar>("javadocJar") {
-                    archiveClassifier.set("javadoc")
-                    from(getByName("dokka"))
-                }
-            }
+        tasks {
+            registerCommonTasks()
         }
     }
 
@@ -160,7 +134,6 @@ kotlin {
         publications.withType<MavenPublication>().apply {
             val jvm by getting {
                 artifactId = "core-jvm"
-                artifact(tasks.getByName("sourcesJar"))
                 artifact(tasks.getByName("javadocJar"))
             }
 
@@ -168,47 +141,10 @@ kotlin {
                 artifactId = "core-common"
             }
         }.forEach {
-            version = if (isRelease) project.version.toString() else "${project.version}-SNAPSHOT"
-
-            it.pom {
-                name.set(project.name)
-                description.set(Info.pomDescription)
-                url.set(if (isRelease) Info.projectUrl else System.getenv("CI_PROJECT_URL"))
-
-                licenses {
-                    license {
-                        name.set(Info.pomLicense)
-                        url.set(Info.pomLicenseUrl)
-                    }
-                }
-                developers {
-                    developer {
-                        email.set(Info.projectDevEmail)
-                    }
-                }
-                organization {
-                    name.set(Info.pomOrg)
-                }
-                scm {
-                    connection.set(if (isRelease) Info.projectCloneUrl else System.getenv("CI_REPOSITORY_URL"))
-                    url.set(if (isRelease) Info.projectUrl else System.getenv("CI_PROJECT_URL"))
-                }
-            }
-
-            signing {
-                if (isRelease) sign(it)
-            }
+            it.configureMavenPom(isRelease, project)
+            signing { if (isRelease) sign(it) }
         }
 
-        repositories {
-            maven {
-                url = uri(if (isRelease) Info.sonatypeReleaseRepoUrl else Info.sonatypeSnapshotRepoUrl)
-
-                credentials {
-                    username = if (isRelease) properties.getProperty("MAVEN_USER") else System.getenv("MAVEN_REPO_USER")
-                    password = if (isRelease) properties.getProperty("MAVEN_PASSWORD") else System.getenv("MAVEN_REPO_PASSWORD")
-                }
-            }
-        }
+        setMavenRepositories(isRelease, properties)
     }
 }
